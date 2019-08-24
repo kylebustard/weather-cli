aff9ad326804fca39a6ca5d622af6263367ce320
Kyle Bustard <bustardkyle@gmail.com>
2019-08-23 19:20:48 -0500

    Add fetchNow and weatherForecast functions

--- src/commands.js
+++ src/commands.js
@@ -1,2 +1,29 @@
+require('dotenv').config()
+const APIXU_KEY = process.env.APIXU_KEY
 const fetch = require('node-fetch');
-const APIXU_KEY = $APIXU_KEY_SECRET
+
+const fetchNow = async (city) => {
+  const response = await fetch(`https://api.apixu.com/v1/current.json?key=${APIXU_KEY}&q=${city}`);
+  const data = await response.json();
+
+  const now = {
+    location: data.location.name,
+    country: data.location.country,
+    longitude: data.location.lon,
+    latitude: data.location.lat,
+    temparature: data.current.temp_c,
+    condition: data.current.condition.text
+  };
+
+  console.log(now);
+}
+
+const weatherForecast = async (city) => {
+  const response = await fetch(`https://api.apixu.com/v1/forecast.json?key=${APIXU_KEY}&q=${city}`);
+
+  const data = await response.json();
+
+  console.log(data.forecast)
+};
+
+module.exports = { fetchNow, weatherForecast };
