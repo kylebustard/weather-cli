e909776274a2c841d983365852bc649015307f42
Kyle Bustard <bustardkyle@gmail.com>
2019-08-23 20:24:56 -0500

    Add more data properties to output

--- src/commands.js
+++ src/commands.js
@@ -5,25 +5,37 @@ const fetch = require('node-fetch');
 const fetchNow = async (city) => {
   const response = await fetch(`https://api.apixu.com/v1/current.json?key=${APIXU_KEY}&q=${city}`);
   const data = await response.json();
+  const { name, region, country, lat, lon } = data.location;
+  const weather = data.current;
 
-  const now = {
-    location: data.location.name,
-    country: data.location.country,
-    longitude: data.location.lon,
-    latitude: data.location.lat,
-    temparature: data.current.temp_f,
-    condition: data.current.condition.text
+  const current = {
+    location: name,
+    region: region,
+    country: country,
+    latitude: lat,
+    longitude: lon,
+    temperature: weather.temp_f,
+    humidity: weather.humidity,
+    temp_feelslike_f: weather.feelslike_f,
+    condition: weather.condition.text
   };
 
-  console.log(now);
+  console.log(current);
 }
 
 const weatherForecast = async (city) => {
   const response = await fetch(`https://api.apixu.com/v1/forecast.json?key=${APIXU_KEY}&q=${city}`);
 
   const data = await response.json();
+  const { date, date_epoch, day, astro } = data.forecast.forecastday[0];
 
-  console.log(data.forecast)
+  const forecast = {
+    date: date,
+    day: day,
+    astro: astro
+  }
+
+  console.log(forecast)
 };
 
 module.exports = { fetchNow, weatherForecast };
