0ce222a787c4ba9da2f730624ed56d656da8ade9
Kyle Bustard <bustardkyle@gmail.com>
2019-08-23 19:21:40 -0500

    Add commands `now` and `forecast`

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-require('dotenv').config()
+const program = require('commander');
+const { fetchNow, weatherForecast } = require('./commands');
+
+program
+  .version('0.0.1')
+  .description('command line weather app')
+
+program
+  .command('now <city')
+  .alias('n')
+  .description('See the current weather in the specified city')
+  .action(city => fetchNow(city));
+
+program
+  .command('forecast <city>')
+  .alias('f')
+  .description('See a weather forecast of a specified city')
+  .action(city => weatherForecast(city));
+
+program.parse(process.argv);
